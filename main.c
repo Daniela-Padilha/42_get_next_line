82a2dcfe0ad09c60af40e9202c772dd27b2a2b0d
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2024-12-16 14:27:46 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,77 @@ int	main(void)
 	int		fd;
 	char	*a;
 
-	fd = open("test.txt", O_RDONLY);
+	fd = open("big.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error: opening file failed");
+		return (1);
+	}
+	a = get_next_line(fd);
+	while (a != NULL)
+	{
+		printf("%s", a);
+		free(a);
+		a = get_next_line(fd);
+	}
+	close(fd);
+	fd = open("oneline.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error: opening file failed");
+		return (1);
+	}
+	a = get_next_line(fd);
+	while (a != NULL)
+	{
+		printf("%s", a);
+		free(a);
+		a = get_next_line(fd);
+	}
+	close(fd);
+	fd = open("notext.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error: opening file failed");
+		return (1);
+	}
+	a = get_next_line(fd);
+	while (a != NULL)
+	{
+		printf("%s", a);
+		free(a);
+		a = get_next_line(fd);
+	}
+	close(fd);
+	fd = open("small.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error: opening file failed");
+		return (1);
+	}
+	a = get_next_line(fd);
+	while (a != NULL)
+	{
+		printf("%s", a);
+		free(a);
+		a = get_next_line(fd);
+	}
+	close(fd);
+	fd = open("ducks.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error: opening file failed");
+		return (1);
+	}
+	a = get_next_line(fd);
+	while (a != NULL)
+	{
+		printf("%s", a);
+		free(a);
+		a = get_next_line(fd);
+	}
+	close(fd);
+	fd = open("dolphin.txt", O_RDONLY);
 	if (fd < 0)
 	{
 		printf("Error: opening file failed");
